1f85997de0b571bfc1553f97f95c4188ef32fef3
senait05 <sghebremichael23@gmail.com>
2024-09-11 17:20:38 -0400

    Update HelloWorld.java
    New sentence added

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,11 @@ class HelloWorld{
  public static void main(String[] args){
  
  System.out.println("Hello World! Yeyy");
+ 
+ String name="Joel Smith";
+ 
+  System.out.println(name);
+  int age =24;
+   System.out.println(age);
 }
 }
