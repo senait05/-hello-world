b9b40839ab1ecb3537069f95b0839162d9839e68
senait05 <sghebremichael23@gmail.com>
2024-09-12 13:34:39 -0400

    Revert "Update HelloWorld.java"
    This reverts commit 1f85997de0b571bfc1553f97f95c4188ef32fef3.

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,11 +4,5 @@ class HelloWorld{
  public static void main(String[] args){
  
  System.out.println("Hello World! Yeyy");
- 
- String name="Joel Smith";
- 
-  System.out.println(name);
-  int age =24;
-   System.out.println(age);
 }
 }
